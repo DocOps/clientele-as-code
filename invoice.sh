29d80043218d8bf04fc267b67c616db8b42b6e43
Brian Dominick <badominick@gmail.com>
2024-10-31 01:39:25 -0400

    Initial commit

---
+++ invoice.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Usage: ./invoice.sh <client-name> [<invoice-id>]"
+  exit 1
+fi
+
+CLIENT_SLUG=$1
+INVOICE_ID=$2
+
+docker run --rm -v "$(pwd):/app" docops-invoice $CLIENT_SLUG $INVOICE_ID
